ec13cdbc321c5dd8538523d7f91044e0c29156e7
dQuigz <dquigz89@gmail.com>
2020-10-06 20:41:34 -0400

    Add files via upload

--- YouPyTwit/youtube/utoobpy_lte.py
+++ YouPyTwit/youtube/utoobpy_lte.py
@@ -14,7 +14,7 @@ def main():
 
 	api_service_name = "youtube"
 	api_version = "v3"
-	client_secrets_file = "yt.config"
+	client_secrets_file = "youtube/yt.config"
 
     # Get credentials and create an API client
 	flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(
